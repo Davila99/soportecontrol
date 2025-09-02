fcce61221068838d66db0dc9d58b1efe35062871
Davila99 <davilaeliseo453@gmail.com>
2025-09-01 21:33:53 -0600

    mejorando filtros admin
    integrando filtros en dedoradores

--- registrolab/admin.py
+++ registrolab/admin.py
@@ -1,8 +1,49 @@
 from django.contrib import admin
-from registrolab.models import RegistroLab,Laboratorio,Asignatura,Carrera,Docente
-# Register your models here.
-admin.site.register(RegistroLab)
-admin.site.register(Laboratorio)
-admin.site.register(Asignatura)
-admin.site.register(Carrera)
-admin.site.register(Docente)
+from .models import RegistroLab, Laboratorio, Asignatura, Carrera, Docente
+
+# ------------------------------
+# Admin para RegistroLab
+# ------------------------------
+@admin.register(RegistroLab)
+class RegistroLabAdmin(admin.ModelAdmin):
+    list_display = ("fecha", "asignatura_id", "docentes_id", "laboratorio_id", "carreras_id", "hora_inicio", "hora_fin")
+    search_fields = (
+        "asignatura_id__descripcion",
+        "docentes_id__nombre",
+        "laboratorio_id__descripcion",
+        "carreras_id__descripcion",
+    )
+    list_filter = ("asignatura_id", "docentes_id", "laboratorio_id", "carreras_id", "fecha")
+    date_hierarchy = "fecha"
+
+# ------------------------------
+# Admin para Laboratorio
+# ------------------------------
+@admin.register(Laboratorio)
+class LaboratorioAdmin(admin.ModelAdmin):
+    list_display = ("id", "descripcion")
+    search_fields = ("descripcion",)
+
+# ------------------------------
+# Admin para Asignatura
+# ------------------------------
+@admin.register(Asignatura)
+class AsignaturaAdmin(admin.ModelAdmin):
+    list_display = ("id", "descripcion")
+    search_fields = ("descripcion",)
+
+# ------------------------------
+# Admin para Carrera
+# ------------------------------
+@admin.register(Carrera)
+class CarreraAdmin(admin.ModelAdmin):
+    list_display = ("id", "descripcion")
+    search_fields = ("descripcion",)
+
+# ------------------------------
+# Admin para Docente
+# ------------------------------
+@admin.register(Docente)
+class DocenteAdmin(admin.ModelAdmin):
+    list_display = ("id", "nombre")
+    search_fields = ("nombre",)
