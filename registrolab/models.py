301ea4c202c638daaa0803f14ca47600639233d9
Bryan18-Alvarado <bryanalvarado1904@gmail.com>
2026-03-12 11:40:48 -0600

    fix: fixing basic error of future duplicate data

--- registrolab/models.py
+++ registrolab/models.py
@@ -1,24 +1,36 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Asignatura(models.Model):
-    descripcion = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=200, unique=True)
+
     def __str__(self):
         return self.descripcion
+
+
 class Laboratorio(models.Model):
-    descripcion = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=200, unique=True)
+
     def __str__(self):
         return self.descripcion
+
+
 class Carrera(models.Model):
-    descripcion = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=200, unique=True)
+
     def __str__(self):
         return self.descripcion
-    
+
+
 class Docente(models.Model):
-    nombre = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=200, unique=True)
+
     def __str__(self):
         return self.nombre
-    
+
+
 class RegistroLab(models.Model):
     fecha = models.DateField('Fecha')
     asignatura_id = models.ForeignKey(Asignatura, on_delete=models.CASCADE)
@@ -27,5 +39,6 @@ class RegistroLab(models.Model):
     carreras_id = models.ForeignKey(Carrera, on_delete=models.CASCADE)
     hora_inicio = models.TimeField('Hora Entrada')
     hora_fin = models.TimeField('Hora Salida')
+
     def __str__(self):
-         return self.fecha.strftime('%Y-%m-%d') + ' ' + self.hora_inicio.strftime('%H:%M') + ' - ' + self.hora_fin.strftime('%H:%M') + ' | ' + str(self.asignatura_id) + ' | ' + str(self.docentes_id) + ' | ' + str(self.laboratorio_id) + ' | ' + str(self.carreras_id)
+        return self.fecha.strftime('%Y-%m-%d') + ' ' + self.hora_inicio.strftime('%H:%M') + ' - ' + self.hora_fin.strftime('%H:%M') + ' | ' + str(self.asignatura_id) + ' | ' + str(self.docentes_id) + ' | ' + str(self.laboratorio_id) + ' | ' + str(self.carreras_id)
